224111ce7a5385aefbb8dbad34ee0919493682bd
Kris <Kris@kingyon.com>
2017-08-18 15:25:18 +0800

    init

---
+++ ImagePickCollectionView/ImagePickCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickCollectionView
+//
+//  Created by XuHuan on 2017/8/18.
+//  Copyright © 2017年 KingYon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
